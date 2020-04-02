5c80411910f4c1a8bcbe6dafee37d3634c94be51
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-02 10:49:20 -0600

    Add form elements and jsx

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -5,14 +5,64 @@ class Form extends Component {
     super(props)
 
     this.state = {
-         
+      name: '',
+      date: '',
+      time: '',
+      guests: ''
     }
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
     return (
-      <div>
-        
+      <div className="form-container">
+        <input 
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          onChange={this.handleInput}
+          required
+          value={name}
+        />
+        <input 
+          type="text"
+          id="date"
+          name="date"
+          placeholder="Date (mm/dd)"
+          onChange={this.handleInput}
+          required
+          value={date}
+        />
+        <input 
+          type="text"
+          id="time"
+          name="time"
+          placeholder="Time"
+          onChange={this.handleInput}
+          required
+          value={time}
+        />
+        <input 
+          type="text"
+          id="guests"
+          name="guests"
+          placeholder="Number of guests"
+          onChange={this.handleInput}
+          required
+          value={guests}
+        />
+        <button
+          type="submit"
+          className="buttons"
+          onClick={() => console.log('reser')}>
+          Make Reservation
+        </button>
       </div>
     )
   }
