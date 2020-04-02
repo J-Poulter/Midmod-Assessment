d1ecde9f40a4edf383eb9ada91e9233b8a95a03a
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-02 09:54:06 -0600

    Implement props in ReservationContainer and map over those props to return individual Reservations cards

--- src/ReservationContainer/ReservationContainer.js
+++ src/ReservationContainer/ReservationContainer.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './ReservationContainer.css';
+import Reservations from '../Reservations/Reservations';
 
-const ReservationContainer = () => {
+const ReservationContainer = (props) => {
+  const renderedReservations = props.reservations.map(reservation => (
+    <Reservations
+      reservation={reservation}
+      key={reservation.id}
+    />
+  ))
   return (
     <div>
-
+      {renderedReservations}
     </div>
   )
 }
