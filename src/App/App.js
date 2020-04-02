e26a50126391826c868eb1409e1bc1b8a45655ae
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-02 09:44:13 -0600

    Add state and fetched data into state

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import ReservationContainer from '../ReservationContainer/ReservationContainer';
 
 class App extends Component {
+ constructor() {
+   super();
+   this.state = {
+     reservations: []
+  }
+ }
+
+ componentDidMount() {
+   fetch('http://localhost:3001/api/v1/reservations')
+   .then(res => res.json())
+   .then(data => this.setState({reservations: data}))
+   .catch(err => console.error(err))
+ }
+  
   render() {
+    const { reservations } = this.state;
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form />
         </div>
         <div className='resy-container'>
-          
+          <ReservationContainer reservations={reservations}/>
         </div>
       </div>
     )
