2512ea55cc2567875bb26a8d14f9cecf898528b6
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-02 10:50:14 -0600

    Add missing elements and jsx to reservation componenets

--- src/ReservationContainer/ReservationContainer.js
+++ src/ReservationContainer/ReservationContainer.js
@@ -10,7 +10,7 @@ const ReservationContainer = (props) => {
     />
   ))
   return (
-    <div>
+    <div className="reservations-container">
       {renderedReservations}
     </div>
   )
--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Reservations.css';
 
-const Reservations = () => {
+const Reservations = ({reservation}) => {
+  const { name, date, time, number } = reservation
   return (
-    <div>
-
+    <div className="reservation-card">
+      <h1 className="res-title">{name}</h1>
+      <p className="res-text">Date: {date}</p>
+      <p className="res-text">{time} pm</p>
+      <p className="res-text">Number of guests: {number}</p>
+      <button 
+        type="button" 
+        onClick={() => console.log('erased')} 
+        className="cancel-button buttons">
+          Cancel
+      </button>
     </div>
   )
 }
