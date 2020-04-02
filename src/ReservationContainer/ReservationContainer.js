8c4d9f414a372dca47563523cb1bae9701fac94d
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-02 09:45:57 -0600

    Create necessarry container files and make the container a functional component

---
+++ src/ReservationContainer/ReservationContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ReservationContainer.css';
+
+const ReservationContainer = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ReservationContainer;
